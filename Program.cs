16cfd0b096309ce124c7dc74681e1f46b47bbb85
Aleksey Smirnov <acwork@list.ru>
2022-09-02 01:27:06 +0300

    Example_1-Example_2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//   System.Console.WriteLine("Vvedite hislo");
+//   int num = Convert.ToInt32(Console.ReadLine());
+//  int resault = num*num; 
+//   System.Console.WriteLine($"Kvadrat chisla {num}->{resault}");
+//  int a= 1;
+
+// System.Console.WriteLine("Введите возводимое в квадрат число");
+// int a = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите результат возведения");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// if (a*a == b)
+// {
+
+// System.Console.WriteLine("Verno");
+
+// }
+
+// else
+// {
+// System.Console.WriteLine("Neverno");
+
+// }
