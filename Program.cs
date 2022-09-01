814f93439ec2b25c27f041f6c39a38e18d58d913
Aleksey Smirnov <acwork@list.ru>
2022-09-02 02:27:36 +0300

    Add Example_02

--- Program.cs
+++ Program.cs
@@ -21,3 +21,56 @@
 // System.Console.WriteLine("Neverno");
 
 // }
+
+
+// Алгоритм дня недели с массивом
+
+// System.Console.WriteLine("Введите число дня недели");
+// int numbday = Convert.ToInt32(Console.ReadLine());
+// string[] nameday = { "Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье"};
+// if ( numbday>=1 & numbday<=7 ){
+// System.Console.WriteLine($"{numbday} день недели это - {nameday[numbday-1]}");
+// // System.Console.Write(nameday[numbday-1]);
+// }
+// else
+// {
+//     System.Console.WriteLine("Введите корректное число");
+// }
+
+
+System.Console.WriteLine("Введите число дня недели");
+int numbday = Convert.ToInt32(Console.ReadLine());
+
+if (numbday == 1)
+{
+    System.Console.WriteLine("Понедельник");
+}
+ else if (numbday == 2)
+{
+    System.Console.WriteLine("Вторник");
+}
+else if (numbday == 3)
+{
+    System.Console.WriteLine("Среда");
+}
+else if (numbday == 4)
+{
+    System.Console.WriteLine("Четверг");
+}
+else if (numbday == 5)
+{
+    System.Console.WriteLine("Пятница");
+}
+else if (numbday == 6)
+{
+    System.Console.WriteLine("Суббота");
+}
+else if (numbday == 7)
+{
+    System.Console.WriteLine("Воскресенье");
+}
+else 
+{
+    System.Console.WriteLine("Введите корректное число");
+
+}
