fda6e525d807998e8e37cc99f83f3698b1da29af
Aleksey Smirnov <acwork@list.ru>
2022-09-02 23:54:14 +0300

    Add 2 examples

--- Program.cs
+++ Program.cs
@@ -23,7 +23,7 @@
 // }
 
 
-// Алгоритм дня недели с массивом
+//  Свое решение  - Алгоритм дня недели с массивом
 
 // System.Console.WriteLine("Введите число дня недели");
 // int numbday = Convert.ToInt32(Console.ReadLine());
@@ -38,39 +38,81 @@
 // }
 
 
-System.Console.WriteLine("Введите число дня недели");
-int numbday = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите число дня недели");
+// int numbday = Convert.ToInt32(Console.ReadLine());
 
-if (numbday == 1)
-{
-    System.Console.WriteLine("Понедельник");
-}
- else if (numbday == 2)
-{
-    System.Console.WriteLine("Вторник");
-}
-else if (numbday == 3)
-{
-    System.Console.WriteLine("Среда");
-}
-else if (numbday == 4)
-{
-    System.Console.WriteLine("Четверг");
-}
-else if (numbday == 5)
-{
-    System.Console.WriteLine("Пятница");
-}
-else if (numbday == 6)
-{
-    System.Console.WriteLine("Суббота");
-}
-else if (numbday == 7)
-{
-    System.Console.WriteLine("Воскресенье");
-}
-else 
-{
-    System.Console.WriteLine("Введите корректное число");
+// if (numbday == 1)
+// {
+//     System.Console.WriteLine("Понедельник");
+// }
+//  else if (numbday == 2)
+// {
+//     System.Console.WriteLine("Вторник");
+// }
+// else if (numbday == 3)
+// {
+//     System.Console.WriteLine("Среда");
+// }
+// else if (numbday == 4)
+// {
+//     System.Console.WriteLine("Четверг");
+// }
+// else if (numbday == 5)
+// {
+//     System.Console.WriteLine("Пятница");
+// }
+// else if (numbday == 6)
+// {
+//     System.Console.WriteLine("Суббота");
+// }
+// else if (numbday == 7)
+// {
+//     System.Console.WriteLine("Воскресенье");
+// }
+// else 
+// {
+//     System.Console.WriteLine("Введите корректное число");
 
-}
+// }
+
+// Все целые числа от -N до N Свое решение с костылями.
+
+// System.Console.WriteLine("Введите число N");
+
+// int N = Convert.ToInt32( Console.ReadLine());
+
+// int step = -N-1; 
+// int sum = 0;
+// System.Console.WriteLine("Числа в диапазоне от -N до N");
+
+// while (step<N)
+// {
+//     sum = step + 1;
+
+//     step = sum;
+    
+//     System.Console.WriteLine(sum);
+// } 
+
+
+// System.Console.WriteLine("Введите число N");
+
+// int N = Convert.ToInt32( Console.ReadLine());
+
+// int step = -N; 
+// System.Console.WriteLine("Числа в диапазоне от -N до N");
+
+// while (step<N)
+// {
+//      System.Console.WriteLine(step);
+//      step++;
+// }
+
+
+System.Console.WriteLine("Введите число");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+int lastDigit = a % 3;
+
+System.Console.WriteLine(lastDigit);
